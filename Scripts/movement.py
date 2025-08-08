0971ef211417284198a64229c183c4a25e12a16f
pjsacchet <psacchet@proton.me>
2025-08-08 13:18:36 -0400

    Flesh out our responses to the device for supposed auth; try to fix program not exiting properly

--- Scripts/movement.py
+++ Scripts/movement.py
@@ -6,8 +6,9 @@
     # The device changes port approx. every 5 minutes, sending packets confirming this roughly every 32 seconds 
 
 # TODO:
-    # Spin up listener thread?
+    # Allow user to choose which port we listen on for the device; change bytes to dynamically generate packet contents for our connect command
     # Look into initial exchange to ensure we dont need any other traffic sent prior to commands 
+    # Fix our program not exiting when entering 'escape' key 
 
 
 import socket
@@ -16,6 +17,8 @@ import time
 from pynput import keyboard
 from enum import Enum
 
+TERMINATE = False
+# Keep count of the number of commands we've sent; the device seems to like to keep track of the current number we're on 
 DIRECTION_COMMAND_COUTNER = 0
 
 # Initial 'connect to us' command (20 bytes)
@@ -25,6 +28,11 @@ CONNECT_COMMAND = b'\xf1\x40\x00\x10\x00\x00' # first six bytes seem standard
 CONNECT_COMMAND_PORT =b'\xb8\x22' # next two bytes are for our port number (little endian) (8888) 
 CONNECT_COMMAND_IP = b'\x01\x89\xa8\xc0' # next four bytes are for our ip address (little endian) (192.168.137.1)
 CONNECT_COMMAND_FOOTER = b'\x00\x00\x00\x00\x00\x00\x00\x00' # last 8 bytes are all 0
+
+# I think the device may prompt us to send auth with this message, dont echo it 
+SEND_AUTH_COMMAND = b'\xf1\x42\x00\x14\x54\x47\x53\x56\x00\x00\x00\x00\x00\x01\x50\xc8\x46\x48\x53\x47\x42\x00\x00\x00'
+# This seems to be send from the device after sending our auth blob 
+AUTH_ACCEPTED = b'\xf1\xd0\x00\x14\xd1\x00\x00\x00\x03\x80\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x3c\x00\x00\x00'
 
 # Maybe an auth blob? idk
 AUTH_BLOB = b'\xf1\xd0\x00\x48\xd1\x00\x00\x00\x02\x80\x00\x00\x3c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x61\x75\x77\x59\x72\x38\x74\x74\x43\x59\x53\x43\x61\x79\x39\x6b\x6e\x52\x54\x7a\x50\x79\x4a\x47\x4e\x69\x54\x31\x54\x6d\x6c\x34\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
@@ -103,6 +111,7 @@ def onPress(key, target_ip, port, sock):
                     print("Sending move right command...")
                     packet = constructDirectionCommandPacket(Direction.RIGHT)                
                 case keyboard.Key.esc:
+                    print("Keypress listening thread exiting...")
                     return False
         sock.sendto(packet, (target_ip, port))
     except socket.error as e:
@@ -122,28 +131,45 @@ def establishSocket() -> socket.socket:
 
 # Listen on our UDP port for packets from our robot 
 def receiveData():
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    while (not TERMINATE):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        sock.bind(("0.0.0.0", 8888))
+            sock.bind(("0.0.0.0", 8888))
 
-        while True:
-            data, addr = sock.recvfrom(1024)
-            print("Recevied " + str(data) + " from " + str(addr[0]) + " on port " + str(addr[1]))
-            sock.sendto(data, (addr[0], int(addr[1]))) # echo back to the device 
-    except socket.error as e:
-        print("ERROR: Failed listener socket " + str(e))
+            while True:
+                data, addr = sock.recvfrom(1024)
+                print("Recevied " + str(data) + " from " + str(addr[0]) + " on port " + str(addr[1]))
+                # Check for our special 'send auth' flag
+                if (data == KEEP_ALIVE_1 or data == KEEP_ALIVE_2):
+                    print("Returning keep alive...")
+                    sock.sendto(data, (addr[0], int(addr[1]))) # echo back to the device 
+                elif (data == SEND_AUTH_COMMAND):
+                    print("Sending auth blob...")
+                    sock.sendto(AUTH_BLOB, (addr[0], int(addr[1]))) 
+                elif (data == AUTH_ACCEPTED):
+                    print("Auth accepted!")
+                else:
+                    print("Not sure what this is; echoing it back...")   
+                    sock.sendto(data, (addr[0], int(addr[1])))
+                
+        except socket.error as e:
+            print("ERROR: Failed listener socket " + str(e))
+    print("Listen thread exiting...")
 
     return
 
 # Do all the things!
 def main():
-    # Grab the target ip and desired port (if any) from user
+    global TERMINATE
+
+    # Grab the target ip and desired port from user
     target_ip = input("Input target ip > ")
     port = input("Input target port > ")
 
     # Spin up a listener thread to get everything the bot sends us 
-    print("Spinning up listening thread...")
+        # Will send appropiate responses depending on what we get/where we are in the setup sequence 
+    print("Spinning up listener thread...")
     listen_thread = threading.Thread(target=receiveData)
 
     listen_thread.start()
@@ -155,19 +181,13 @@ def main():
         # A lot of data we receive will need to be echoed back I fear
     sock.sendto(constructConnectCommandPacket(), (target_ip, int(port)))
 
-    time.sleep(.5) # wait to send our auth blob
-
-    # Send this 'auth blob' type thing we always send on connect 
-    sock.sendto(AUTH_BLOB, (target_ip, int(port)))
-    sock.sendto(AUTH_BLOB, (target_ip, int(port)))
-    sock.sendto(AUTH_BLOB, (target_ip, int(port)))
-    sock.sendto(AUTH_BLOB, (target_ip, int(port)))
-
-    # Send ok after we get the all clear?
+    print("Spinning up keypress listener thread... (press ESC to exit)")
 
     # Keep taking input from user, use arrow keys to map to head movement 
     with keyboard.Listener(on_press=lambda event:onPress(event, target_ip=target_ip, port=int(port), sock=sock)) as listener:
         listener.join()
+        # When the key press thread is exiting, that means the user is done so signal our listening thread to exit
+        TERMINATE = True
         listen_thread.join()
         
 
